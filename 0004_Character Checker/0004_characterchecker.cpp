d184776943b2c3fa19b48b6313fbea5d18aaa4c5
Siriphak Amrapal <supperhappyjoey@gmail.com>
2022-02-17 23:18:41 +0700

    str lower upper check

---
+++ 0004_Character Checker/0004_characterchecker.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char in_str[10001];
+    int note_char;
+    int check_cs;
+
+    scanf("%s", &in_str);
+
+    if ((in_str[0] >= 'A') && (in_str[0] <= 'Z'))
+        note_char = 2;
+    else if((in_str[0] >= 'a') && (in_str[0] <= 'z'))
+        note_char = 1;
+
+    for(int i = 1; in_str[i] != '\0'; i++)
+    {
+        if ((in_str[i] >= 'A') && (in_str[i] <= 'Z'))
+            check_cs = 2;
+        else if((in_str[i] >= 'a') && (in_str[i] <= 'z'))
+            check_cs = 1;
+
+        if (note_char != check_cs)
+        {
+            note_char = 0; 
+            break;
+        }
+    }
+    printf("\nnote_char = %d", note_char);
+
+    return (0);
+}
